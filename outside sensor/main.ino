f68efa2f0b68c1e731f609c2f10f39fcf3e4bcef
himikat123 <himikat123@gmail.com>
2017-05-31 21:40:31 +0300

    Add files via upload

--- outside sensor/main.ino
+++ outside sensor/main.ino
@@ -1,9 +1,8 @@
 /* Outside sensor v1.0
- * © Alexandr Piteli himikat123@gmail.com, Chisinau, Moldova, 2017 
- * http://esp8266.atwebpages.com
- */
+   © Alexandr Piteli himikat123@gmail.com, Chisinau, Moldova, 2017
+   http://esp8266.atwebpages.com
+*/
 
-                               // Board ESP-12E (128kB SPIFFS) 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -16,40 +15,39 @@
 #include <pgmspace.h>
 #include "SparkFunBME280.h"
 #include "Wire.h"
-#include <Adafruit_NeoPixel.h>
 
-extern "C"{
-  #include "main.h"
-  #include "languages.h"
+extern "C" {
+#include "main.h"
+#include "languages.h"
 }
 
 #define site "http://esp8266.atwebpages.com/api/"
 #define BUTTON         0
-#define LED            15
-#define BRIGHTNESS     50
+#define R              12
+#define G              13
+#define B              14
 
 ESP8266WebServer webServer(80);
 File fsUploadFile;
 BME280 mySensor;
-Adafruit_NeoPixel strip=Adafruit_NeoPixel(1,LED,NEO_GRB+NEO_KHZ800);
 
 void setup() {
-   //WS2812B
-  strip.begin();
-  strip.setPixelColor(0,strip.Color(150,150,0));
-  strip.show();
+   //PINs
+  pinMode(R, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(B, OUTPUT);
    //Serial port
   Serial.begin(115200);
   while (!Serial);
-   //SPIFS
-  if(!SPIFFS.begin()) while(1) yield();
-   //EEPROM
-  Serial.println("read settings"); 
+  //SPIFS
+  if (!SPIFFS.begin()) while (1) yield();
+  //EEPROM
+  Serial.println("read settings");
   read_eeprom();
   Serial.println("----------------------------------");
-   //PINs
-  pinMode(BUTTON,INPUT);
-   //WIFI MODE
+  //PINs
+  pinMode(BUTTON, INPUT);
+  //WIFI MODE
   WiFi.mode(WIFI_STA);
    //BME280
   mySensor.settings.commInterface=I2C_MODE;
@@ -62,21 +60,15 @@ void setup() {
   mySensor.settings.humidOverSample=1;
   delay(10);
   mySensor.begin();
-  Serial.print("Temperature: ");Serial.print(mySensor.readTempC(),2);Serial.println(" degrees C");
-  Serial.print("Pressure: ");Serial.print(mySensor.readFloatPressure(),2);Serial.println(" Pa");
-  Serial.print("%RH: ");Serial.print(mySensor.readFloatHumidity(),2);Serial.println(" %");
+  Serial.print("Temperature: "); Serial.print(mySensor.readTempC(), 2); Serial.println(" degrees C");
+  Serial.print("Pressure: "); Serial.print(mySensor.readFloatPressure(), 2); Serial.println(" Pa");
+  Serial.print("%RH: "); Serial.print(mySensor.readFloatHumidity(), 2); Serial.println(" %");
   Serial.println("-----------------------------------");
    //battery
   Serial.println("read battery level");
   bat_level=BatteryLevel();
-  if(bat_level==1){
-    strip.setPixelColor(0,strip.Color(150,0,0));
-    strip.show();
-  }
-  else{
-    strip.setPixelColor(0,strip.Color(0,150,0));
-    strip.show();
-  }  
+  if(bat_level==1) led(1,0,0);
+  else led(0,1,0);
   Serial.println("-----------------------------------");
    // WiFi
   html.ssid.toCharArray(ssid,(html.ssid.length())+1);
@@ -86,8 +78,7 @@ void setup() {
     Serial.print("\" with password \""); Serial.print(rtcData.AP_PASS); Serial.println("\"");
     WiFi.mode(WIFI_AP_STA);
     WiFi.softAP(rtcData.AP_SSID,rtcData.AP_PASS);
-    strip.setPixelColor(0,strip.Color(150,0,150));
-    strip.show();
+    led(1,0,1);
     web_settings();
     while(1){
       webServer.handleClient();
@@ -99,29 +90,27 @@ void setup() {
       IPAddress ip;
       IPAddress subnet;
       IPAddress gateway;
-      if(ip.fromString(html.ip) and gateway.fromString(html.gateway) and subnet.fromString(html.mask)){
+      if(ip.fromString(html.ip) and gateway.fromString(html.gateway) and subnet.fromString(html.mask)) {
         WiFi.config(ip,gateway,subnet);
       }
     }
     uint8_t e=0;
     Serial.print("connecting to "); Serial.println(ssid);
     while(WiFi.status()!=WL_CONNECTED){
-      if(e&1) strip.setPixelColor(0,strip.Color(150,150,0));
+      if(e&1) led(1,1,0);
       else{
-        if(bat_level==1) strip.setPixelColor(0,strip.Color(150,0,0));
-        else strip.setPixelColor(0,strip.Color(150,0,0));
+        if(bat_level==1) led(1,0,0);
+        else led(0,1,0);
       }
-      strip.show(); 
       if((e++)>50){
         Serial.print("unable to connect to "); Serial.println(ssid);
         is_settings();
-        strip.setPixelColor(0,strip.Color(0,0,0));
-        strip.show();
+        led(0,0,0);
         mySensor.settings.runMode=0;
         ESP.deepSleep(1200000000);
       }
       delay(500);
-    } 
+    }
   }
   Serial.print("connected to "); Serial.println(ssid);
   String IP=WiFi.localIP().toString();
@@ -136,9 +125,9 @@ void loop(){
     Serial.print(html.ssid);
     Serial.println(" is connected");
     Serial.println("---------------------------");
-    Serial.print("Latitude ");Serial.println(latitude);
-    Serial.print("Longitude ");Serial.println(longitude);
-    Serial.print("Altitude ");Serial.println(altitude);
+    Serial.print("Latitude "); Serial.println(latitude);
+    Serial.print("Longitude "); Serial.println(longitude);
+    Serial.print("Altitude "); Serial.println(altitude);
     String mac=WiFi.macAddress();
     float temp=mySensor.readTempC();
     url=site;
@@ -153,7 +142,7 @@ void loop(){
     url+="&TEMP=";
     url+=temp;
     url+="&PRES=";
-    url+=mySensor.readFloatPressure()/100;
+    url+= mySensor.readFloatPressure()/100;
     url+="&HUM=";
     url+=mySensor.readFloatHumidity();
     url+="&BAT=";
@@ -171,14 +160,12 @@ void loop(){
     Serial.print("Server returned: ");
     Serial.println(httpData);
     client.end();
-    strip.setPixelColor(0,strip.Color(0,0,150));
-    strip.show();  
+    led(0,0,1);
     httpData="";
     is_settings();
     delay(1000);
     Serial.println("going sleep");
-    strip.setPixelColor(0,strip.Color(0,0,0));
-    strip.show();
+    led(0,0,0);
     mySensor.settings.runMode=0;
     ESP.deepSleep(1200000000);
   }
@@ -188,11 +175,15 @@ void loop(){
     Serial.println(" was not found");
     is_settings();
     Serial.println("going sleep");
-    strip.setPixelColor(0,strip.Color(0,0,0));
-    strip.show();
+    led(0,0,0);
     mySensor.settings.runMode=0;
     ESP.deepSleep(120000000);
   }
+}
+void led(bool r,bool g,bool b){
+  digitalWrite(R,!r);
+  digitalWrite(G,!g);
+  digitalWrite(B,!b);
 }
 
 void is_settings(void){
@@ -201,13 +192,11 @@ void is_settings(void){
     Serial.println("entering settings mode");
     WiFi.mode(WIFI_AP_STA);
     WiFi.softAP(rtcData.AP_SSID,rtcData.AP_PASS);
-    Serial.printf("connect to %s, password is %s\r\n",rtcData.AP_SSID,rtcData.AP_PASS);
+    Serial.printf("connect to %s, password is %s\r\n", rtcData.AP_SSID,rtcData.AP_PASS);
     Serial.println("type 192.168.4.1 in address bar of your browser");
     String IP=WiFi.localIP().toString();
-    Serial.print("alt IP is ");
-    Serial.println(IP);
-    strip.setPixelColor(0,strip.Color(150,0,150));
-    strip.show();  
+    Serial.print("alt IP is "); Serial.println(IP);
+    led(1,0,1);
     web_settings();
     while(1){
       webServer.handleClient();
@@ -223,19 +212,19 @@ int viewNetworks(void){
     for(uint8_t i=0;i<n;i++){
       delay(1);
       if(WiFi.SSID(i)==html.ssid) rssi=WiFi.RSSI(i);
-      Serial.print(WiFi.SSID(i));Serial.print(" ");
+      Serial.print(WiFi.SSID(i)); Serial.print(" ");
       Serial.println(WiFi.RSSI(i));
-    }  
+    }
   }
   return rssi;
 }
 
 void read_eeprom(void){
-  ESP.rtcUserMemoryRead(0,(uint32_t*) &rtcData,sizeof(rtcData));
-  if(rtcData.crc_ssid!=calculateCRC32(((uint8_t*) &rtcData.AP_SSID),sizeof(rtcData.AP_SSID))) strcpy(rtcData.AP_SSID,DEFAULT_AP_SSID);
-  if(rtcData.crc_pass!=calculateCRC32(((uint8_t*) &rtcData.AP_PASS),sizeof(rtcData.AP_PASS))) strcpy(rtcData.AP_PASS,DEFAULT_AP_PASS);
-  Serial.printf("AP_SSID=%s AP_PASS=%s\r\n",rtcData.AP_SSID,rtcData.AP_PASS);
-  
+  ESP.rtcUserMemoryRead(0,(uint32_t*)&rtcData,sizeof(rtcData));
+  /*if(rtcData.crc_ssid!=calculateCRC32(((uint8_t*) &rtcData.AP_SSID),sizeof(rtcData.AP_SSID))) */strcpy(rtcData.AP_SSID, DEFAULT_AP_SSID);
+  /*if(rtcData.crc_pass!=calculateCRC32(((uint8_t*) &rtcData.AP_PASS),sizeof(rtcData.AP_PASS))) */strcpy(rtcData.AP_PASS, DEFAULT_AP_PASS);
+  //Serial.printf("AP_SSID=%s AP_PASS=%s\r\n",rtcData.AP_SSID,rtcData.AP_PASS);
+
   String fData;
   File f=SPIFFS.open("/bat.json","r");
   if(f){
@@ -244,12 +233,12 @@ void read_eeprom(void){
     DynamicJsonBuffer jsonBuf;
     JsonObject& json=jsonBuf.parseObject(fData);
     if(json.success()){
-      battery.min=json["min"];  
+      battery.min=json["min"];
       battery.max=json["max"];
       Serial.printf("bat min=%d bat max=%d\r\n",battery.min,battery.max);
     }
   }
-    
+
   String fileData;
   File file=SPIFFS.open("/settings.json","r");
   if(file){
@@ -258,7 +247,7 @@ void read_eeprom(void){
     DynamicJsonBuffer jsonBuffer;
     JsonObject& root=jsonBuffer.parseObject(fileData);
     if(root.success()){
-      String ssid=root["ssid"];  
+      String ssid=root["ssid"];
       String pass=root["pass"];
       String ip=root["ip"];
       String mask=root["mask"];
@@ -279,12 +268,12 @@ void read_eeprom(void){
   Serial.print("CONN TYPE="); Serial.println(html.typ);
   Serial.print("IP="); Serial.println(html.ip);
   Serial.print("MASK="); Serial.println(html.mask);
-  Serial.print("GATEWAY="); Serial.println(html.gateway);  
+  Serial.print("GATEWAY="); Serial.println(html.gateway);
 }
 
 void save_eeprom(void){
   rtcData.crc_ssid=calculateCRC32(((uint8_t*) &rtcData.AP_SSID),sizeof(rtcData.AP_SSID));
-  rtcData.crc_pass=calculateCRC32(((uint8_t*) &rtcData.AP_PASS),sizeof(rtcData.AP_PASS));  
+  rtcData.crc_pass=calculateCRC32(((uint8_t*) &rtcData.AP_PASS),sizeof(rtcData.AP_PASS));
   ESP.rtcUserMemoryWrite(0,(uint32_t*) &rtcData,sizeof(rtcData));
 
   DynamicJsonBuffer jsonBuffer;
@@ -299,7 +288,7 @@ void save_eeprom(void){
   File file=SPIFFS.open("/settings.json","w");
   if(file){
     root.printTo(file);
-    file.close();  
+    file.close();
   }
 }
 
@@ -311,7 +300,7 @@ uint32_t calculateCRC32(const uint8_t *data,size_t length){
       bool bit=crc&0x80000000;
       if(c&i) bit=!bit;
       crc<<=1;
-      if(bit) crc^=0x04c11db7;
+      if(bit)crc^=0x04c11db7;
     }
   }
   return crc;
@@ -321,20 +310,20 @@ int BatteryLevel(void){
   int level_battery=analogRead(A0);
   if(level_battery>battery.max){
     battery.max=level_battery;
-    save_bat();  
+    save_bat();
   }
   if(level_battery<battery.min){
     battery.min=level_battery;
     save_bat();
   }
-  
+
   int b1=(battery.max-battery.min);
   int b2=(battery.max+1-level_battery);
   int b3=round(b1/b2);
   if(b3>5) b3=5;
   Serial.printf("ADC=%d\r\n",level_battery);
   Serial.printf("level=%d\r\n",b3);
-  return b3;  
+  return b3;
 }
 
 void save_bat(void){
